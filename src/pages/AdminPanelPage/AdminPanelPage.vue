1c230713ec94e80cfcfdb531dc27596de6661fcc
ivanov9826 <ivanov9826@gmail.com>
2023-10-10 15:58:30 +0300

    Separate admin form to new file , add page folder

---
+++ src/pages/AdminPanelPage/AdminPanelPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Admin Panel Page</h1>
+  </div>
+  <div>
+    <RegisterForm />
+  </div>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import RegisterForm from "./RegisterForm.vue";
+</script>
+
+<style scoped lang="scss"></style>

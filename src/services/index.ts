75fc7c037816f7f2080cb8301c6ec566d01dac2f
Viktoria Dzivkova <viktoriad12@icloud.com>
2023-10-10 21:44:21 +0300

    Add menu component and store, including crud logic

--- src/services/index.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-// import constants from "@/constants.js"
-
-export default axios.create({
-//   baseURL: 
-})
--- src/services/menuServices/menuServices.ts
+++ src/services/menuServices/menuServices.ts
@@ -1,27 +1,76 @@
-class Menu {
-    constructor(
-        public name: string,
-        public price: number,
-        public description: string,
-        public id?: string
-    ) { }
+import fireBaseData from "../firebaseConfig";
+
+export class Menu {
+  constructor(
+    public id: string,
+    public name: string,
+    public price: number,
+    public description: string
+  ) {}
 }
-
-
-
-
-
 
 function validateMenu(menuItem: Menu) {
-    if(!menuItem.name) {
-        throw new Error('Add proper dish name!')
-    }
+  if (!menuItem.name) {
+    throw new Error("Add proper dish name!");
+  }
 
-    if(!menuItem.description) {
-        throw new Error('Add proper description for the dish!')
-    }
+  if (!menuItem.description) {
+    throw new Error("Add proper description for the dish!");
+  }
 
-    if(!menuItem.price || menuItem.price < 0) {
-        throw new Error('Add valid and non negative price for the dish!')
-    }
+  if (!menuItem.price || menuItem.price < 0) {
+    throw new Error("Add valid and non negative price for the dish!");
+  }
 }
+
+export default {
+  async getAll() {
+    try {
+      const data: Menu[] = [];
+      const querySnapshot = await fireBaseData.fireStore
+        .collection("menu")
+        .get();
+
+      querySnapshot.forEach((doc: any) => {
+        const { id, name, price, description } = doc.data();
+        const menu = new Menu(id, name, price, description);
+        console.log(menu);
+        data.push(menu);
+      });
+
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Error fetching menu:", error);
+      throw error;
+    }
+  },
+
+  async addProduct(product: Menu) {
+    await fireBaseData.fireStore.collection("menu").add({
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      description: product.description,
+    });
+  },
+
+  async deleteProduct(product: Menu) {
+    const docRef = fireBaseData.fireStore.collection("menu").doc(product.id);
+    try {
+      await docRef.delete();
+      console.log("Product deleted successfully");
+    } catch (error) {
+      console.error("Error deleting product:", error);
+    }
+  },
+
+  async editProduct(product:Menu){
+    await fireBaseData.fireStore.collection("menu").doc(product.id).update({
+    name: product.name,
+    price: product.price,
+    description: product.description,
+  });
+  }
+  
+};

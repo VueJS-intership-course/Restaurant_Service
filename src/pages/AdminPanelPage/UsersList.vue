952c36cd744b022da0cd9175c0d38e1bbac90a3f
ivanov9826 <ivanov9826@gmail.com>
2023-10-10 17:02:59 +0300

    Fetch the users and update user list

--- src/pages/AdminPanelPage/UsersList.vue
+++ src/pages/AdminPanelPage/UsersList.vue
@@ -4,10 +4,21 @@
   </div>
   <div>
     <ul>
-      <li>
-        <p>UserName</p>
-        <p>User Email</p>
+      <li v-for="user in users" :key="user.email">
+        <p>{{ user.username }}</p>
+        <p>{{ user.role }}</p>
       </li>
     </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+import userServices from "../../services/userServices/userServices";
+import { ref, onMounted } from "vue";
+
+const users = ref();
+
+onMounted(async () => {
+  users.value = await userServices.getAll();
+});
+</script>

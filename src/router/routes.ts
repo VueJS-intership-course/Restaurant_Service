2c5aca0a4a056caafb78e9c578a0e8331b5eb836
Viktoria Dzivkova <viktoriad12@icloud.com>
2023-10-10 21:44:21 +0300

    Add menu component and store, including crud logic

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,34 +2,35 @@ import HomePage from "../pages/HomePage.vue";
 import AboutUsPage from "../pages/AboutUsPage.vue";
 import AdminPanelPage from "../pages/AdminPanelPage.vue";
 import EmployeeLoginPage from "../pages/EmployeeLoginPage.vue";
-import MenuPage from "../pages/MenuPage.vue";
+import MenuPage from "../pages/menu-page/MenuComponent.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "entry",
-    component: HomePage,
-  },
-  {
-    path: "/about",
-    name: "aboutUs",
-    component: AboutUsPage,
-  },
-  {
-    path: "/control-panel",
-    name: "controlPanel",
-    component: AdminPanelPage,
-  },
-  {
-    path: "/login",
-    name: "employeeLogin",
-    component: EmployeeLoginPage,
-  },
-  {
-    path: "/menu",
-    name: "menu",
-    component: MenuPage,
-  },
-];
+    {
+      path: "/",
+      name: "entry",
+      component: HomePage,
+    },
+    {
+      path: "/about",
+      name: "aboutUs",
+      component: AboutUsPage,
+    },
+    {
+      path: "/control-panel",
+      name: "controlPanel",
+      component: AdminPanelPage,
+    },
+ 
+    {
+      path: "/login",
+      name: "employeeLogin",
+      component: EmployeeLoginPage,
+    },
+    {
+      path: "/menu",
+      name: "menu",
+      component: MenuPage
+    }
+  ];
 
-export default routes;
+  export default routes;
---
+++ src/store/productStore.ts
@@ -0,0 +1,38 @@
+import { defineStore } from "pinia";
+import menuServices from "../services/menuServices/menuServices";
+import { Menu } from "../services/menuServices/menuServices";
+
+export const useProductStore = defineStore({
+  id: "productStore",
+  state: () => ({
+    products: [] as Menu[],
+  }),
+
+  getters: {
+    isAdmin: (state) => {
+      return true;
+    },
+
+  },
+
+  actions: {
+    async getProductList() {
+      this.products = await menuServices.getAll();
+    },
+
+    async addProduct(product: Menu) {
+      await menuServices.addProduct(product);
+      this.getProductList();
+    },
+
+    async removeProduct(product: Menu) {
+      await menuServices.deleteProduct(product);
+      this.getProductList();
+    },
+
+    async editProduct(product: Menu) {
+      await menuServices.editProduct(product);
+      this.getProductList();
+    },
+  },
+});

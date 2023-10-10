7b38c1f2a2395685d7c4fa212ecbd55d6e44003b
Gogena01 <gogi2013_gogi2013@abv.bg>
2023-10-10 14:34:17 +0300

    Updating User Services

--- src/services/userServices/userServices.ts
+++ src/services/userServices/userServices.ts
@@ -4,9 +4,8 @@ class Employee {
     constructor(
         public username: string,
         public password: string,
-        public name: string,
         public email: string,
-        public role: string,
+        public role: 'employee' | 'admin' | 'manager',
         public id?: string,
     ) { }
 }
@@ -37,10 +36,11 @@ export default {
             validateSchema(user)
             const userCredential = await fireBaseData.fireAuth.createUserWithEmailAndPassword(user.email, user.password);
 
+
             if (userCredential && userCredential.user) {
                 await fireBaseData.fireStore.collection('user-profiles').doc(userCredential.user.uid).set({
+                    email: user.email,
                     username: user.username,
-                    name: user.name,
                     role: user.role,
                 });
             }
@@ -68,20 +68,15 @@ function validateSchema(user: Employee) {
         throw new Error('Invalid email address');
     }
 
-    if (user.password.length < 8) {
+    if (user.password.length < 8 || user.password) {
         throw new Error('Password must be at least 8 characters long');
     }
 
-    if (!user.name.trim()) {
-        throw new Error('Name cannot be empty');
+    if (user.role) {
+        throw new Error('Invalid role choice');
     }
 
-    const validRoles = ['employee', 'admin', 'manager'];
-    if (!validRoles.includes(user.role)) {
-        throw new Error('Invalid role');
-    }
-
-    if (!user.username.trim()) {
+    if (!user.username || !user.username.trim()) {
         throw new Error('Username cannot be empty');
     }
 }

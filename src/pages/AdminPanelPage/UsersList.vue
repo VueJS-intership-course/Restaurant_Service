451e3e54ace3f8c9cfe9735602fb8f6c07a6d690
ivanov9826 <ivanov9826@gmail.com>
2023-10-11 20:44:22 +0300

    Creating table for the user list

--- src/pages/AdminPanelPage/UsersList.vue
+++ src/pages/AdminPanelPage/UsersList.vue
@@ -3,23 +3,31 @@
     <h3>Users list</h3>
   </div>
   <div>
-    <ul>
-      <li v-for="user in store.userList" :key="user.email">
-        <p>{{ user.username }}</p>
-        <p>{{ user.email }}</p>
-      </li>
-    </ul>
+    <table>
+      <tr>
+        <th v-for="(label, index) in columnLabels" :key="index">{{ label }}</th>
+      </tr>
+      <tr v-for="user in store.userList" :key="user.username">
+        <td>{{ user.id }}</td>
+        <td>{{ user.username }}</td>
+        <td>{{ user.email }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script setup lang="ts">
 // import userServices from "../../services/userServices/userServices";
+import { ref } from "vue";
+import type { Ref } from "vue";
 import { onMounted } from "vue";
 import { usersStore } from "../../store/usersStore";
 
 // const users = ref();
 
 const store = usersStore();
+
+const columnLabels: Ref<string[]> = ref(["ID", "Username", "Email"]);
 
 onMounted(async () => {
   store.getAllUsers();
@@ -29,6 +37,14 @@ onMounted(async () => {
 <style lang="scss" scoped>
 li {
   list-style-type: none;
+  border: 1px solid black;
+}
+
+table {
+  border: 1px solid black;
+}
+
+td {
   border: 1px solid black;
 }
 </style>

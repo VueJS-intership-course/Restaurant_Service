64711685ea5c02c558545bec13c53d4f2324e1c1
Viktoria Dzivkova <viktoriad12@icloud.com>
2023-10-11 10:21:33 +0300

    fix edit and delete functionality for menu items

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import AboutUsPage from "../pages/AboutUsPage.vue";
-import AdminPanelPage from "../pages/AdminPanelPage.vue";
+import AdminPanelPage from "../pages/AdminPanelPage/AdminPanelPage.vue";
 import EmployeeLoginPage from "../pages/EmployeeLoginPage.vue";
 import MenuPage from "../pages/menu-page/MenuComponent.vue";
 

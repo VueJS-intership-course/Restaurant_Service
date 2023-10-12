7a57ed2f3ea8663edb28bd223dc720859fed3ee1
ivanov9826 <ivanov9826@gmail.com>
2023-10-12 13:11:25 +0300

    Testing for error prompt

--- src/services/orderServices/orderServices.ts
+++ src/services/orderServices/orderServices.ts
@@ -1,22 +1,22 @@
-import fireBaseData from '../firebaseConfig';
-import { Menu } from '../menuServices/menuServices';
-import {useOrderStore} from '../../store/orderStore.ts';
-
+import fireBaseData from "../firebaseConfig";
+import { Menu } from "../menuServices/menuServices";
+import { useOrderStore } from "../../store/orderStore.ts";
+//Trolololo
 export class Orders {
   constructor(
-    public status: 'pending' | 'done' | 'confirmed',
+    public status: "pending" | "done" | "confirmed",
     public items: Menu[],
-    public createdAt: Date,
+    public createdAt: Date
   ) {}
 }
 
 function validateOrders(order: Orders) {
   if (!order.status) {
-    throw new Error('Invalid order status!');
+    throw new Error("Invalid order status!");
   }
 
   if (!order.items.length) {
-    throw new Error('Invalid order items!');
+    throw new Error("Invalid order items!");
   }
 }
 
@@ -24,19 +24,14 @@ export default {
   async finishOrder() {
     try {
       const orderStore = useOrderStore();
-      const order = new Orders(
-        'pending',
-        orderStore.orderItems,
-        new Date(),
-      );
-      
+      const order = new Orders("pending", orderStore.orderItems, new Date());
+
       validateOrders(order);
 
-      console.log('Fire');
-      await fireBaseData.fireStore.collection('orders').add(order);
-  
+      console.log("Fire");
+      await fireBaseData.fireStore.collection("orders").add(order);
     } catch (error) {
-      console.error('Error placing the order:', error);
+      console.error("Error placing the order:", error);
     }
   },
 };

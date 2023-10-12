fabc2f207baf802a8d2194125d6c2cdb24efeb64
Gogena01 <gogi2013_gogi2013@abv.bg>
2023-10-12 14:53:42 +0300

    Fixing ts errors for login validate rules

--- src/pages/Authentication/EmployeeLoginPage.vue
+++ src/pages/Authentication/EmployeeLoginPage.vue
@@ -1,35 +1,45 @@
 <template>
-    <Form @submit="signIn">
-        <div>
-            <h1>Sign In</h1>
-        </div>
-        <div class="form-inputs">
-            <div class="form-inputs_username">
-                <label for="email">Email:</label>
-                <Field type="email" name="email" :rules="emailRules" />
-                <ErrorMessage name="email" />
-            </div>
-            <div class="form-inputs_password">
-                <label for="password">Password:</label>
-                <Field name="password" type="password" :rules="passwordRules" />
-                <ErrorMessage name="password" />
-            </div>
-        </div>
-        <div class="form-controls">
-            <button-component type="submit" btn-style="default-button-small">Submit</button-component>
-        </div>
-    </Form>
+  <Form @submit="signIn">
+    <div>
+      <h1>Sign In</h1>
+    </div>
+    <div class="form-inputs">
+      <div class="form-inputs_username">
+        <label for="email">Email:</label>
+        <Field type="email" name="email" :rules="(emailRules as RuleExpression<unknown>)" />
+        <ErrorMessage name="email" />
+      </div>
+      <div class="form-inputs_password">
+        <label for="password">Password:</label>
+        <Field name="password" type="password" :rules="(passwordRules as RuleExpression<unknown>)" />
+        <ErrorMessage name="password" />
+      </div>
+    </div>
+    <div class="form-controls">
+      <button-component type="submit" btn-style="default-button-small">Submit</button-component>
+    </div>
+  </Form>
 </template>
 
 <script setup lang="ts">
+/*
+    imports
+*/
 import { ref } from 'vue';
 import ButtonComponent from '../../common-templates/ButtonComponent.vue';
 import userServices from '../../services/userServices/userServices';
 import { useRouter } from 'vue-router';
-import { Field, Form, ErrorMessage } from 'vee-validate';
+import { Field, Form, ErrorMessage, GenericObject, RuleExpression } from 'vee-validate';
+
+/*
+    router
+*/
 
 const router = useRouter();
 
+/*
+    validation
+*/
 
 const errorMsg = ref<string>('');
 
@@ -41,7 +51,6 @@ const emailRules = (value: string) => {
   if (!regex.test(value)) {
     return 'Invalid email entered!';
   }
-
   return true
 };
 
@@ -55,7 +64,11 @@ const passwordRules = (value: string) => {
   return true
 };
 
-const signIn = (values: { email: string, password: string }) => {
+/*
+    sign in
+*/
+
+const signIn = <T extends GenericObject>(values: T): void => {
   try {
     userServices.signIn(values.email, values.password);
     router.push({ path: 'menu' });
@@ -71,39 +84,39 @@ const signIn = (values: { email: string, password: string }) => {
 @import '../../styles/variables';
 
 h1 {
-    color: $yellow
+  color: $yellow
 }
 
 form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  padding: 3rem 6rem;
+  background-color: $green;
+  position: absolute;
+  top: 35%;
+  left: 35%;
+  border-radius: 8%;
+
+  span {
+    color: red;
+    font-size: large;
+  }
+
+  .form-inputs {
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
-    padding: 3rem 6rem;
-    background-color: $green;
-    position: absolute;
-    top: 35%;
-    left: 35%;
-    border-radius: 8%;
+    gap: 0.5rem;
 
-    span {
-        color: red;
-        font-size: large;
+    .form-inputs_username {
+      @include input-label
     }
 
-    .form-inputs {
-        display: flex;
-        flex-direction: column;
-        gap: 0.5rem;
-
-        .form-inputs_username {
-            @include input-label
-        }
-
-        .form-inputs_password {
-            @include input-label
-        }
+    .form-inputs_password {
+      @include input-label
     }
+  }
 }
 </style>

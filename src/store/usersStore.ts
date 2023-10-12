d372ef1237cb2e31c12523fa39ca88fb05182fb1
ivanov9826 <ivanov9826@gmail.com>
2023-10-12 14:14:26 +0300

    Deleting test form file

--- src/store/usersStore.ts
+++ src/store/usersStore.ts
@@ -6,10 +6,16 @@ export const usersStore = defineStore({
   id: "usersStore",
   state: () => ({
     userList: [] as Employee[],
+    currentUser: {},
   }),
   actions: {
     async getAllUsers() {
       this.userList = await userServices.getAll();
+    },
+
+    setCurrentUser(user: any) {
+      this.currentUser = user;
+      console.log(this.currentUser);
     },
   },
 });

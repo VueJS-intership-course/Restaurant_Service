d5f9afe2f93ceb1af2967249200d98171d180105
Gogena01 <gogi2013_gogi2013@abv.bg>
2023-10-13 09:24:56 +0300

    Adding Base version of AboutUs Page

--- src/pages/AboutUsPage.vue
+++ src/pages/AboutUsPage.vue
@@ -1,8 +1,80 @@
 <template>
-    <h1>About Us Page</h1>
+    <div class="aboutUs">
+        <div class="aboutUs-headings">
+            <h1>For foodlovers, by foodlovers</h1>
+            <h2>We are the fastest growing restaurant in Plovdiv distric.We are known for the quality of our food.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, libero quia esse molestias accusantium ut quasi nisi
+                dignissimos nemo ratione et autem sit nostrum excepturi quos quis? Facere, rerum eum.</h2>
+        </div>
+        <div class="aboutUs-images">
+            <div class="aboutUs-images_singleItem">
+                <router-link :to="{path:'menu'}"><img src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/pouring-honey-on-pancakes.jpg" alt="" /></router-link>
+            </div>
+
+            <div class="aboutUs-images_singleItem">
+                <router-link :to="{path:'menu'}"><img src="https://www.foodiesfeed.com/wp-content/uploads/2023/07/fresh-fruit-platter.jpg" alt="" /></router-link>
+            </div>
+
+            <div class="aboutUs-images_singleItem">
+               <router-link :to="{path:'menu'}"><img src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/vegan-curry-with-fresh-herbs.jpg" alt="" /></router-link>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
+
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '../styles/variables';
+
+.aboutUs {
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+    .aboutUs-headings {
+    display: flex;
+    position: relative;
+    width: 50%;
+    left: 24%;
+    padding: 2rem;
+    flex-direction: column;
+    text-align: center;
+    justify-content: center;
+    text-wrap: balance;
+    align-items: center;
+    flex-wrap: wrap;
+    background-color: $yellow;
+
+    h1 {
+        color:$dark-green;
+        font-weight: bolder;
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    }
+}
+
+.aboutUs-images {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+    background-color: $green;
+    padding: 1.5rem;
+    .aboutUs-images_singleItem {
+        img {
+            width: 400px;
+            height: 300px;
+            background-repeat: no-repeat;
+            object-fit: cover;
+            transition: all 0.6s;
+        }
+
+        img:hover {
+            transform: translateY(-5%);
+        }
+    }
+}
+}
+
+
+</style>
--- src/pages/Authentication/EmployeeLoginPage.vue
+++ src/pages/Authentication/EmployeeLoginPage.vue
@@ -91,13 +91,13 @@ form {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: center;    
   gap: 1rem;
   padding: 3rem 6rem;
   background-color: $green;
   position: absolute;
   top: 35%;
-  left: 35%;
+  left: 40%;
   border-radius: 8%;
 
   span {

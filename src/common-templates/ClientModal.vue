d857694d816181ff76de4b8fe2fbd1d60a4ee9e5
ivanov9826 <ivanov9826@gmail.com>
2023-10-16 16:13:06 +0300

    Add validation to client login

--- src/common-templates/ClientModal.vue
+++ src/common-templates/ClientModal.vue
@@ -1,56 +1,73 @@
 <template>
-    <div class="modal">
-        <Form class="form" @submit="handleSubmit">
-            <div>
-                <h1>Enter information for your order</h1>
-            </div>
-            <div class="form-inputs">
-                <div class="form-inputs_username">
-                    <label for="name">Name:</label>
-                    <Field type="text" name="name"  />
-                    <ErrorMessage name="name" />
-                </div>
-                <div class="form-inputs_password">
-                    <label for="phone">Phone:</label>
-                    <Field name="phone" type="tel" />
-                    <ErrorMessage name="phone" />
-                </div>
-            </div>
-            <div class="form-controls">
-                <button-component type="submit" btn-style="default-button-small">Submit</button-component>
-            </div>
-        </Form>
-    </div>
+  <div class="modal">
+    <Form class="form" @submit="handleSubmit" :validation-schema="schema">
+      <div>
+        <h1>Enter information for your order</h1>
+      </div>
+      <div class="form-inputs">
+        <div class="form-inputs_username">
+          <label for="name">Name:</label>
+          <Field type="text" name="name" />
+          <ErrorMessage name="name" />
+        </div>
+        <div class="form-inputs_password">
+          <label for="phone">Phone:</label>
+          <Field name="phone" type="tel" />
+          <ErrorMessage name="phone" />
+        </div>
+      </div>
+      <div class="form-controls">
+        <button-component type="submit" btn-style="default-button-small"
+          >Submit</button-component
+        >
+      </div>
+    </Form>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { Field, Form, ErrorMessage, GenericObject } from 'vee-validate';
-import ButtonComponent from './ButtonComponent.vue';
-import { useRouter } from 'vue-router';
+import { Field, Form, ErrorMessage, GenericObject } from "vee-validate";
+import ButtonComponent from "./ButtonComponent.vue";
+import { useRouter } from "vue-router";
+import * as yup from "yup";
 
-const emits = defineEmits(['close-modal']);
-const router = useRouter()
+const emits = defineEmits(["close-modal"]);
+const router = useRouter();
 
-const handleSubmit = <T extends GenericObject>(values:T) => {
-   localStorage.setItem('user', JSON.stringify(values));
-   emits('close-modal');
-   router.push({name:'menu'})
-}
+const handleSubmit = <T extends GenericObject>(values: T) => {
+  localStorage.setItem("user", JSON.stringify(values));
+  emits("close-modal");
+  router.push({ name: "menu" });
+};
 
+const schema = yup.object({
+  name: yup
+    .string()
+    .required("This field is required!")
+    .min(2, "Name should be atleast two symbols!"),
+  phone: yup
+    .number()
+    .required("This field is required!")
+    .min(10, "Please enter a valid phone number!"),
+});
 </script>
 
 <style scoped lang="scss">
-@import '../styles/variables';
+@import "../styles/variables";
 
 .modal {
-    @include modal-style;
+  @include modal-style;
 
-    .form {
-        @include client-form
-    }
+  .form {
+    @include client-form;
+  }
 
-    .form-inputs {
-        @include input-label
-    }
+  .form-inputs {
+    @include input-label;
+  }
+
+  span {
+    color: red;
+  }
 }
 </style>

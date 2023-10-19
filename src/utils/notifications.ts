e2b61a7914f04da197c608227051014f309b1816
ivanov9826 <ivanov9826@gmail.com>
2023-10-19 15:07:57 +0300

    Removing comments

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,5 +1,5 @@
 import { toast } from "vue3-toastify";
-// import "vue3-toastify/dist/index.css";
+import "vue3-toastify/dist/index.css";
 
 const showNotification = (msg: string) => {
   toast(msg, {
